c3589f006c8fd82e9601edd7b20a001dfae1424b
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-03-28 09:18:35 -0300

    senha e usuário

--- main.go
+++ main.go
@@ -3,17 +3,17 @@ package main
 import "fmt" 
 
 func main (){
-	var number1 int
-	var number2 int
-	fmt.Print("Type one number:")
-	fmt.Scan(&number1)
-	fmt.Print("type another number:")
-	fmt.Scan(&number2)
+	var user string
+	var password string
 
-	fmt.Println("The sum is:", number1 + number2)
-	fmt.Println("The subtraction is:", number1 - number2)
-	fmt.Println("The division is:", number1 / number2)
-	fmt.Println("The multiplication is:", number1 * number2)
-	fmt.Println("The rest of the division is:", number1 % number2)
+	fmt.Print("Enter your username:")
+	fmt.Scan(&user)
+	fmt.Print("Type your password:")
+	fmt.Scan(&password) 
 
-	}
+	if user != "admin" && password != "1234" {
+	fmt.Println("Access denied") 
+    }else{
+	fmt.Println("Access allowed") 
+   }
+}
