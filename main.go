fc1b83d3d8d91c13f3d2f0f1426cd85de889ce86
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-04-03 08:54:00 -0300

    vetor superhero

--- main.go
+++ main.go
@@ -9,4 +9,19 @@ func main (){
 	fmt.Println(names)
 	names[3] = "Rogério"
 	fmt.Println(names)
+	// Slice
+	var score = []int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+	var superhero = []string{"Deadpool", "Spiderman", "Ironman"}
+	fmt.Println(superhero)
+	superhero = append(superhero, "BlackPanter")
+	fmt.Println(superhero, len(superhero), cap(superhero))
 }
