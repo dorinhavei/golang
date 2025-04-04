c5fdfa165bc2fe273136dfc024259cf779ebfadf
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-04-04 08:24:44 -0300

    stand library

--- main.go
+++ main.go
@@ -1,27 +1,24 @@
 package main
 
-import "fmt" 
-
-func main (){
-	var ages = [4]int{17,16,20,40}
-	names:= [4]string{"Olivia", "Isabel", "Rodrigo", "Liv"}
-    fmt.Println(ages)
-	fmt.Println(names)
-	names[3] = "Rogério"
-	fmt.Println(names)
-	// Slice
-	var score = []int{100, 200, 300, 400}
-	fmt.Println(score)
-	score[1] = 2
-	fmt.Println(score, len(score), cap(score))
-	rangeOne := score[1:3]
-	fmt.Println(rangeOne)
-	rangeTwo := score[2:]
-	fmt.Println(rangeTwo)
-	rangeThree := score[:3]
-	fmt.Println(rangeThree)
-	var superhero = []string{"Deadpool", "Spiderman", "Ironman"}
-	fmt.Println(superhero)
-	superhero = append(superhero, "BlackPanter")
-	fmt.Println(superhero, len(superhero), cap(superhero))
+import (
+  "strings"
+  "fmt"
+  "sort"
+)
+func main() {
+   greeting := "Hello guys!"
+   fmt.Println(strings.Contains(greeting, "friends"))
+   fmt.Println(strings.ReplaceAll(greeting,"Hello", "Hi"))
+   fmt.Println(strings.ToUpper(greeting))
+   fmt.Println(strings.Index(greeting, "my"))
+   fmt.Println(strings.Split(greeting, " "))
+   ages := []int{50, 80, 10}
+   sort.Ints(ages)
+   fmt.Println(ages)
+   index := sort.SearchInts(ages, 50)
+   fmt.Println(index)
+   names := []string{"Olivia", "Isabel", "Rodrigo"}
+   sort.Strings(names)
+   fmt.Println(names)
+   fmt.Println(sort.SearchStrings(names, "Olivia"))
 }
