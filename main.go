233ea309498860dad38f164ad700c720759c4b9f
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-04-17 14:35:27 -0300

    grades

--- main.go
+++ main.go
@@ -1,27 +1,23 @@
 package main
 
-import "fmt"
-
-// dadosPessoa recebe o nome e a idade de uma pessoa e retorna a mesma idade
-// e se ela é maior ou menor de idade.
-func dadosPessoa(nome string, idade int) (int, string) {
-    // Verifica se a idade é maior ou igual a 18
-    if idade >= 18 {
-        return idade, "Maior de idade"
-    }
-    return idade, "Menor de idade"
-}
+import (
+    "fmt"
+)
 
 func main() {
-    // Exemplo de uso da função dadosPessoa
-    nome := "Olivia"
-    idade := 23
-
-    // Chama a função e recebe a idade e a classificação
-    idadeCalculada, classificacao := dadosPessoa(nome, idade)
-
-    // Exibe os resultados
-    fmt.Printf("Nome: %s\n", nome)
-    fmt.Printf("Idade: %d\n", idadeCalculada)
-    fmt.Println(classificacao)
+    studentAge := make(map[string]int)
+    studentAge["Olivia"] = 16
+    studentAge["Isabel"] = 23
+    studentAge["Rodrigo"] = 21
+    studentAge["Maria"] = 16
+    fmt.Println("Olivia's age", studentAge["Olivia"])
+    studentGrade := map[string]float64{
+        "Clara" : 10, 
+        "Olivia" : 9.7,
+        "Isabel" : 8.5,
+        "Maria" : 9.9,
+    }
+    for k,v := range studentGrade{
+        fmt.Printf("%s got a grade %.1f \n", k, v)
+    }
 }
