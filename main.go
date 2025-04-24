790affb5f658e511a72af67ce9ab0cf457a53724
isa.dora.farias4601@gmail.com <isa.dora.farias4601@gmail.com>
2025-04-24 13:25:55 -0300

    getName

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
-import (
-    "fmt"
-)
+import  "fmt";
 
-func main() {
-    studentAge := make(map[string]int)
-    studentAge["Olivia"] = 16
-    studentAge["Isabel"] = 23
-    studentAge["Rodrigo"] = 21
-    studentAge["Maria"] = 16
-    fmt.Println("Olivia's age", studentAge["Olivia"])
-    studentGrade := map[string]float64{
-        "Clara" : 10, 
-        "Olivia" : 9.7,
-        "Isabel" : 8.5,
-        "Maria" : 9.9,
-    }
-    for k,v := range studentGrade{
-        fmt.Printf("%s got a grade %.1f \n", k, v)
-    }
+func getName() (string, string){
+  return "Olivia", "Rodrigo"
+}
+func main(){
+    name, lastname := getName()
+    fmt.Println(name);
+    fmt.Println(lastname)   
+
 }
